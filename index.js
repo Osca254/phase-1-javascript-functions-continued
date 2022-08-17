59e06600b2daebd233300bab4acb88e561383aca
moringa <oscarokumu47@gmail.com>
2022-08-17 17:57:51 +0300

    modify index.js && package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(){
+    return("This Saturday, I want to roller-skate!");
+}saturdayFun();
+function mondayWork(){
+  return ("This Monday, I will go to the office.");
+}
+mondayWork();
+
+function wrapAdjective(){
+    
+}
